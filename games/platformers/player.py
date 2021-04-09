8b1f5f61a377e6788b14b8b810403ae8b77412ec
Ranovan7 <romyranovan@gmail.com>
2021-04-09 14:44:53 +0700

    decompose Player.update()

--- games/platformers/player.py
+++ games/platformers/player.py
@@ -25,6 +25,18 @@ class Player(pygame.sprite.Sprite):
 
         pressed_keys = pygame.key.get_pressed()
 
+        self.process_input(pressed_keys)
+
+        self.acc.x += self.vel.x * FRIC
+        self.acc.y += self.vel.y * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        self.check_border()
+
+        self.rect.midbottom = self.pos
+
+    def process_input(pressed_keys):
         if pressed_keys[K_LEFT]:
             self.acc.x = -ACC
         if pressed_keys[K_RIGHT]:
@@ -34,11 +46,7 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_DOWN]:
             self.acc.y = ACC
 
-        self.acc.x += self.vel.x * FRIC
-        self.acc.y += self.vel.y * FRIC
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-
+    def check_border():
         if self.pos.x > WIDTH - self.horizontal_limit:
             self.pos.x = WIDTH - self.horizontal_limit
         if self.pos.x < self.horizontal_limit:
@@ -47,5 +55,3 @@ class Player(pygame.sprite.Sprite):
             self.pos.y = HEIGHT
         if self.pos.y < self.surf.get_height():
             self.pos.y = self.surf.get_height()
-
-        self.rect.midbottom = self.pos
