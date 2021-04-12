f42b40fdfd23d8f5b6778d99cc49d5c439f82adf
Ranovan7 <romyranovan@gmail.com>
2021-04-12 17:17:41 +0700

    added another nim example

--- nimrod/__init__.py
+++ nimrod/__init__.py
@@ -11,6 +11,11 @@ def looper(n: int):
     return result[n-10:]
 
 
+def looper_str(text: str, subtext:str, n_iter: int):
+    for i in range(n_iter):
+        subtext in text
+
+
 def try_nim():
     print(nim_math.add(2, 4))
 
@@ -30,4 +35,27 @@ def try_nim():
     start = time.time()
     py_fib = looper(n_fib)
     print(f"Results using Python : {len(py_fib)}")
+    print(f"-- execution time : {round(time.time() - start, 3)} seconds\n")
+
+    text = '''
+I think this very thing is not like the thing that I think it is
+I would be happy to eat a banana collected from an apple tree, though I am not really sure
+it would taste like a banana - rather like an apple. What's the point in eating a banana that
+tastes like an apple? Or, for that matter, eating an apple that tastes like a banana?
+
+Some day I will explain you everything in detail. Today, gotta go since I have some performance
+comparisons to do. Bye!
+    '''
+    subtext = "gotta go since"
+
+    n_iter = 5_000_000
+
+    start = time.time()
+    nim_fib = nim_math.looper_str(text, subtext, n_iter)
+    print(f"Results using Nim")
+    print(f"-- execution time : {round(time.time() - start, 3)} seconds\n")
+
+    start = time.time()
+    py_fib = looper_str(text, subtext, n_iter)
+    print(f"Results using Python")
     print(f"-- execution time : {round(time.time() - start, 3)} seconds\n")
--- nimrod/nim_math.nim
+++ nimrod/nim_math.nim
@@ -1,4 +1,9 @@
 import nimpy
+import times, strutils
+
+proc strstr(haystack, needle: cstring): cstring {.importc, header: "<string.h>".}
+
+proc contains(haystack, needle: string): bool = strstr(cstring(haystack), cstring(needle)) != nil
 
 proc add(a: int, b: int): int {.exportpy.} =
     return a + b
@@ -13,3 +18,9 @@ proc looper(n: int): seq[int] {.exportpy.} =
         result.add(i*2)
 
     return result[^10..^1]
+
+proc looper_str(text: string, subtext: string, n_iter: int): void {.exportpy.} =
+    let time = cpuTime()
+    for i in 0..n_iter:
+        discard subtext in text
+    echo "Time taken: ", cpuTime() - time
